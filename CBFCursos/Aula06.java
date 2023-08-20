faa0bf8cc7b5c78cec1bdb5b3a102122b19daa44
FrancoBorba <franco.borba14@gmail.com>
2023-08-20 19:45:37 -0300

    new

--- CBFCursos/Aula06.java
+++ CBFCursos/Aula06.java
@@ -1,13 +1,18 @@
 package CBFCursos;
-// aray em java
+// array em java
     import java.util.*;
 public class Aula06 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int num[] = new int[5]; // declaração de array
+        int numeros[] = { 2,5,7,8,11}; // declarando array com numeros ja definidos
+        
+        for(int i = 0 ; i < num.length ; i++){ // array.lenght , retorna o tamanho do array
+            num[i] = scan.nextInt(); 
+        } // ler array 
         for(int i = 0 ; i < 5 ; i++){
-            num = scan.nextInt();
-        }
-        System.out.printf("%d",num[0]);
+            System.out.printf("%d \n",num[i]);
+        }// imprimir array
+        
     }
 }
