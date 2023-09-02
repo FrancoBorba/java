da87cc0cd8e1e1643f5d27d97f14a2a9f8751a2f
FrancoBorba <franco.borba14@gmail.com>
2023-09-02 15:46:42 -0300

    new

---
+++ CBFCursos/Classes/Animal.java
@@ -0,0 +1,12 @@
+package CBFCursos.Classes;
+// a clase animal vai inplementar a classe SerVivo
+public class Animal implements SerVivo{
+
+    public Animal(){
+        
+    }
+
+    public void mover(){}
+    public void comer(int massa){}
+    public void info(){}
+}
---
+++ CBFCursos/Classes/SerVivo.java
@@ -0,0 +1,7 @@
+package CBFCursos.Classes;
+// classe para aula 18 - INTERFACe
+public interface SerVivo {
+    public void mover();
+    public void comer(int massa);
+    public void info();
+}
