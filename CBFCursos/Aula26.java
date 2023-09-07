ccf939540c6db91250e63a25b7b0a968b45cfe0b
FrancoBorba <franco.borba14@gmail.com>
2023-09-07 11:41:30 -0300

    Desafio aula 26

--- CBFCursos/Aula26.java
+++ CBFCursos/Aula26.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Aula26 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String nome;
+        
         final int numPerguntas= 4;
         char [] gabarito = {'c','c' ,'a','a'};
 
@@ -24,5 +24,25 @@ public class Aula26 {
 
         };
         char[] respostas = new char[numPerguntas];
+        char resp;
+        String nome;
+        double nota = 0;
+        System.out.println("Digite seu nome");
+        nome = scan.nextLine();
+        for ( int i = 0 ; i < numPerguntas ; i++){
+            System.out.println("------------------------");
+            System.out.printf("Pergunta %d \n",i+1);
+            System.out.printf("%s \n",perguntas[i]);
+            System.out.printf("%s \n",alternativas[i]);
+            resp = scan.nextLine().charAt(0);
+            respostas[i] = resp;
+        }
+        for( int i = 0 ; i < numPerguntas ; i++){
+            if(gabarito[i]== respostas[i]){
+                nota = nota + 2.5;
+            }
+        }
+        System.out.printf(" %s sua nota foi %.2f , voce foi %s ",nome,nota, nota >= 6 ? "Aprovado" : "Reprovado");
+        scan.close();
     }
 }
