6e834210528ed5df4210a9130fe4fc94b0f3773a
FrancoBorba <franco.borba14@gmail.com>
2023-10-07 12:37:39 -0300

    Questao 2 - Lista 7 Array

--- Listas/Lista7Array/ex002.java
+++ Listas/Lista7Array/ex002.java
@@ -1,5 +1,22 @@
 package Listas.Lista7Array;
-
+import java.security.SecureRandom;;
 public class ex002 {
-    
+    public static void main(String[] args) {
+        final int tam = 10;
+        double maior = 0.0;
+        double vet[]  = new double[tam];
+        for( int i = 0 ; i < tam; i++){
+            vet[i] = new SecureRandom().nextDouble(100);
+        }
+        for( int i = 0 ; i < tam ; i++){
+            System.out.printf("%.1f - ",vet[i]);
+        }
+        System.out.println();
+        for( int i = 0 ; i <tam ; i++){
+            if( vet[i] > maior){
+                maior = vet[i];
+            }
+        }
+        System.out.println(" O maior elemento do vetor = "+ maior);
+     }
 }
