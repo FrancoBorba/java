01d94d7177220bd21e2244601475629318946052
FrancoBorba <franco.borba14@gmail.com>
2023-11-12 21:22:58 -0300

    Alterações na questão
    Tentarei melhorar a questão depois

--- Listas/ListaRelacaoDeClasses/Carro/App.java
+++ Listas/ListaRelacaoDeClasses/Carro/App.java
@@ -1,18 +1,16 @@
 package Listas.ListaRelacaoDeClasses.Carro;
 
+
+
 public class App {
     public static void main(String[] args) {
-        Motor m1 = new Motor();
-        m1.setFabricante("Ferrari");
-        m1.setPotencia(100);
-        m1.setPreco(1000000);
-       // System.out.println(m1);
-        Pneu p1 = new Pneu("pirelli", "soft", 20000);
-       // System.out.println(p1);
-        Banco b1 = new Banco("ferrari", "urus", 15000);
-       // System.out.println(b1);
-        Carro ferrari = new Carro(p1, m1, b1);
-        System.out.println("Preço do carro:" + ferrari.precoCarro());
+       Motor m1 = new Motor("Ferrari", 2000000, 420);
+       Banco b1 = new Banco("Ferrari", "confortavel", 15000);
+       Pneu p1 = new Pneu("Pirelli", "soft", 20000);
+
+       Carro c1 = new Carro(p1, m1, b1);
+       System.out.println(c1.precoCarro());
+       
 
     }
 }
