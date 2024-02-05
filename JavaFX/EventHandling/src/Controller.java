2da8d8df7bbfaef71626e7bf0b7defbfe1cff349
FrancoBorba <franco.borba14@gmail.com>
2024-02-05 16:30:33 -0300

    Utilizando Buttons
    Utilizando o scene builder para criar scenes e buttons e conectar as ações dos buttons com ações geradas

--- JavaFX/EventHandling/src/Controller.java
+++ JavaFX/EventHandling/src/Controller.java
@@ -1,17 +1,34 @@
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
 
 public class Controller {
-    public void up(ActionEvent e){
-        System.out.println("up");
+    @FXML
+    private Circle circle;
+    private double x;
+    private double y;
+    @FXML
+    void btnDown(ActionEvent event) {
+       // System.out.println("down");
+       circle.setCenterY(y+=4);
     }
-    public void down(ActionEvent e){
-        System.out.println("down");
-    }
-    public void left(ActionEvent e){
-        System.out.println("left");
-    }
-    public void right(ActionEvent e){
-        System.out.println("right");
-    }
-}
 
+    @FXML
+    void btnLeft(ActionEvent event) {
+        //System.out.println("left");
+        circle.setCenterX(x -= 4);
+    }
+
+    @FXML
+    void btnRight(ActionEvent event) {
+        //System.out.println("Right");
+        circle.setCenterX(x += 4);
+    }
+
+    @FXML
+    void btnUp(ActionEvent event) {
+       // System.out.println("Up");
+       circle.setCenterY(y-=4);
+    }
+
+}
