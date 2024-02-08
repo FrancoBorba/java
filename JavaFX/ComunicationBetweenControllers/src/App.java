dd5cbf5b368c5de1481b67c4e7b08b51f5230fb6
FrancoBorba <franco.borba14@gmail.com>
2024-02-07 21:39:30 -0300

    Começo da aula
    aula para comunicação de controles , apenas comecei e nao consegui terminar pois tenho que sair

---
+++ JavaFX/ComunicationBetweenControllers/src/App.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("scene1.fxml"));
+        Scene scene = new Scene(root);
+
+        Image icon = new Image(getClass().getResourceAsStream("icon.png"));
+
+        String css = this.getClass().getResource("estilo.css").toExternalForm();
+        scene.getStylesheets().add(css);
+
+        primaryStage.setTitle("Aula sobre controles");
+        primaryStage.getIcons().add(icon);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
--- JavaFX/SwitchScenes/src/Controller.java
+++ JavaFX/SwitchScenes/src/Controller.java
@@ -6,7 +6,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+ 
 public class Controller {
     private Stage stage;
     private Scene scene;
@@ -27,4 +27,23 @@ public class Controller {
         stage.show();
     }
 }
+// Este medoto está sendo usado quando as telas tem o mesmo controller , logo pelo controller eu faço as trocas de tela
 
+/*
+e.getSource(): e é um parâmetro do tipo ActionEvent, que é comumente usado para eventos de
+interface do usuário, como cliques em botões. getSource() retorna o objeto que acionou o
+evento, que no seu caso seria um nó da interface gráfica (por exemplo, um botão).
+
+• ((Node)e.getSource()): Este trecho converte o objeto obtido em um tipo Node, que é uma classe
+base para todos os elementos de interface gráfica no JavaFX.
+
+• .getScene(): Obtém a cena à qual o nó pertence. A cena é basicamente um contêiner para o
+conteúdo gráfico em JavaFX.
+
+• .getWindow(): Obtém a janela (window) à qual a cena está associada. Uma janela é uma área
+retangular na tela onde a interface gráfica é exibida.
+
+• (Stage)((Node)e.getSource()).getScene().getWindow(): Finalmente, converte a janela obtida para
+o tipo Stage. Um Stage é uma janela JavaFX principal que geralmente contém uma ou mais
+cenas. 
+*/
