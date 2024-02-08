8233aded33b33a245496dd8a7842fcf07d9ac286
FrancoBorba <franco.borba14@gmail.com>
2024-02-08 14:25:33 -0300

    Termino do codigo
    Está dando erro em alguma parte , tenho que consertar ainda

---
+++ JavaFX/ComunicationBetweenControllers/src/Scene1Controller.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class Scene1Controller {
+
+    @FXML
+    TextField nameTextFild;
+
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void login(ActionEvent e) throws IOException{
+        
+        String username = nameTextFild.getText();
+    
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("scene2.fxml"));
+        root = loader.load();
+
+        Scene2Controller scene2Controller = loader.getController();
+        scene2Controller.displayName(username);
+
+
+        // root = FXMLLoader.load(getClass().getResource("scene2.fxml"));
+        // este codigo abaixo troca as linhas
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
