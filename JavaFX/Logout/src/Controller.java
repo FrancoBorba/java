cbc95a97e0fcd90082a8e539b1c40c3636ec76fd
FrancoBorba <franco.borba14@gmail.com>
2024-02-08 21:30:32 -0300

    Descrição sobre tipos de alert

--- JavaFX/Logout/src/Controller.java
+++ JavaFX/Logout/src/Controller.java
@@ -33,3 +33,18 @@ public class Controller {
 
 }
 }
+/*
+Como funciona o Alert: primeiramente temos que criar o Alert e declara seu tipo, nesse caso foi
+criado com o tipo Alert.Alerttype.CONFIRMATION, logo é uma janela que possui um botão para
+confirmar ou cancelar, aqui estão alguns tipos de Alert:
+• Information: ◦ Tipo: Alert.AlertType.INFORMATION ◦ Descrição: Uma caixa de diálogo informativa que exibe uma mensagem simples.
+• Confirmation: ◦ Tipos: Alert.AlertType.CONFIRMATION ◦ Descrição: Uma caixa de diálogo de confirmação usada para obter a confirmação do
+usuário.
+• Warning: ◦ Tipo: Alert.AlertType.WARNING ◦ Descrição: Uma caixa de diálogo de aviso para alertar o usuário sobre uma situação
+potencialmente problemática.
+• Error: ◦ Tipo: Alert.AlertType.ERROR ◦ Descrição: Uma caixa de diálogo de erro para exibir mensagens de erro ao usuário.
+• None: ◦ Tipo: Alert.AlertType.NONE ◦ Descrição: Um alerta personalizado que não exibe um ícone padrão.
+• TextInputDialog: ◦ Descrição: Uma caixa de diálogo que permite ao usuário inserir um texto.
+• ChoiceDialog: ◦ Descrição: Uma caixa de diálogo que permite ao usuário escolher uma opção a partir de uma
+lista de opções.
+ */
