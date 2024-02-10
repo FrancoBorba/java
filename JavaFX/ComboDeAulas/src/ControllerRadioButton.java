43fb8a425f252f373c0079d8bdd00995e92312b6
FrancoBorba <franco.borba14@gmail.com>
2024-02-09 22:05:22 -0300

    Preparando proxima Scene

--- JavaFX/ComboDeAulas/src/ControllerRadioButton.java
+++ JavaFX/ComboDeAulas/src/ControllerRadioButton.java
@@ -1,9 +1,16 @@
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 // Radio Buttons devem ser adicionados a um Toggle Group , assim não se pode marcar mais de uma vez
 public class ControllerRadioButton {
 
@@ -27,7 +34,9 @@ public class ControllerRadioButton {
     Image acai = new Image(getClass().getResourceAsStream("/imagens/Acai.jpg"));
     Image teste = new Image(getClass().getResourceAsStream("/imagens/lightOn.jpg"));
 
-
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
   
    public void getFood(ActionEvent event) {
@@ -41,6 +50,20 @@ public class ControllerRadioButton {
         if(acaiRadio.isSelected()){
             imageView.setImage(acai);
         }
+    }
+      public void backScene(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("/view/CheckBoxScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+       scene = new Scene(root);
+       stage.setScene(scene);
+       stage.show();
+    }
+    public void nextScene(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("/view/DataPickerScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
   
 }
