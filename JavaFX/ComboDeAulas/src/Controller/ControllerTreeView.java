d5e4d7a68f6a14a673e8770bc256bdfc986b39b2
FrancoBorba <franco.borba14@gmail.com>
2024-02-12 19:12:04 -0300

    Adicionando Buttons

--- JavaFX/ComboDeAulas/src/Controller/ControllerTreeView.java
+++ JavaFX/ComboDeAulas/src/Controller/ControllerTreeView.java
@@ -1,18 +1,29 @@
 package Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.stage.Stage;
 
 
 public class ControllerTreeView implements Initializable {
 
     @FXML
     private TreeView<String> treeView;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     @FXML
     void selectItem() {
@@ -49,6 +60,20 @@ public class ControllerTreeView implements Initializable {
       treeView.setRoot(rootItem);
 
       
+    }
+        public void backScene(ActionEvent event) throws IOException{
+         root = FXMLLoader.load(getClass().getResource("/view/ListViewScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+       scene = new Scene(root);
+       stage.setScene(scene);
+       stage.show();
+    }
+    public void nextScene(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("/view/MenuBarScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
