a679488754e523ffc333a074019115ed61938da7
FrancoBorba <franco.borba14@gmail.com>
2024-02-15 17:54:30 -0300

    MediaView node

---
+++ JavaFX/MediaView/src/Controller.java
@@ -0,0 +1,50 @@
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.util.Duration;
+
+public class Controller implements Initializable{
+   
+    @FXML
+    private MediaView mediaView;
+
+    @FXML
+    private Button pauseButton;
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    private Button resetButton;
+
+    private File file;
+    private Media media;
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        file = new File("CrazyFrog.mp4");
+        media = new Media(file.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+    }
+    public void playMedia(ActionEvent event){
+        mediaPlayer.play();
+    }
+    public void pauseMedia(ActionEvent event){
+        mediaPlayer.pause();
+    }
+    public void resetMedia(ActionEvent event){
+        mediaPlayer.seek(Duration.seconds(0.0));
+    }
+    
+
+}
